421e9eb41befa12ef6e921991275d00d472fc1d9
wataru <c0b22003e0@edu.teu.ac.jp>
2023-12-19 17:13:59 +0900

    ゲージを描画、増加させる

--- Banging_Splash.py
+++ Banging_Splash.py
@@ -7,27 +7,29 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 # screen.fill((150, 150, 150))
-pygame.display.set_caption('Invaders Game')
+pygame.display.set_caption('ex05/Invaders Game')
 
 # Player
-playerImg = pygame.image.load('player.png')
+playerImg = pygame.image.load('ex05/player.png')
 playerX, playerY = 370, 480
 playerX_change = 0
 
 # Enemy
-enemyImg = pygame.image.load('enemy.png')
+enemyImg = pygame.image.load('ex05/enemy.png')
 enemyX = random.randint(0, 736)
 enemyY = random.randint(50, 150)
 enemyX_change, enemyY_change = 4, 40
 
 # Bullet
-bulletImg = pygame.image.load('bullet.png')
+bulletImg = pygame.image.load('ex05/bullet.png')
 bulletX, bulletY = 0, 480
 bulletX_change, bulletY_change = 0, 3
 bullet_state = 'ready'
 
 # Score
 score_value = 0
+
+
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -46,6 +48,14 @@ def isCollision(enemyX, enemyY, bulletX, bulletY):
         return True
     else:
         return False
+    
+#gaugeの値設定
+gauge_value = 0
+gauge_max = 3
+#gaugeの描画設定
+def gauge():
+    pygame.draw.rect(screen, (255, 0, 0), [20, 80, gauge_max * 20, 20])  #　赤のゲージ
+    pygame.draw.rect(screen, (0, 255, 0), [20, 80, gauge_value * 20, 20])  # 緑のゲージ
 
 running = True
 while running:
@@ -80,10 +90,10 @@ while running:
         break
     enemyX += enemyX_change
     if enemyX <= 0: #左端に来たら
-        enemyX_change = 4
+        enemyX_change = 1#4
         enemyY += enemyY_change
     elif enemyX >=736: #右端に来たら
-        enemyX_change = -4
+        enemyX_change = -1#-4
         enemyY += enemyY_change
 
     collision = isCollision(enemyX, enemyY, bulletX, bulletY)
@@ -91,8 +101,13 @@ while running:
         bulletY = 480
         bullet_state = 'ready'
         score_value += 1
+        if gauge_value<gauge_max:
+            gauge_value += 1 # ゲージの値を1増加する。
+        if gauge_value >= gauge_max:
+            gauge_value=gauge_max
         enemyX = random.randint(0, 736)
         enemyY = random.randint(50, 150)
+
 
     # Bullet Movement
     if bulletY <=0:
@@ -107,6 +122,10 @@ while running:
     font = pygame.font.SysFont(None, 32) # フォントの作成　Noneはデフォルトのfreesansbold.ttf
     score = font.render(f"Score : {str(score_value)}", True, (255,255,255)) # テキストを描画したSurfaceの作成
     screen.blit(score, (20,50))
+  
+
+    # guageの描画
+    gauge()
 
     player(playerX, playerY)
     enemy(enemyX, enemyY)
