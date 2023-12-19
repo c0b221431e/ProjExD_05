4ba6d0d1689f7925c640d9dba9aa4ace8c95bbaa
c0a22017 <c0a2201755@edu.teu.ac.jp>
2023-12-19 17:15:18 +0900

    シールド完成

--- Banging_Splash.py
+++ Banging_Splash.py
@@ -10,21 +10,27 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption('Invaders Game')
 
 # Player
-playerImg = pygame.image.load('player.png')
+playerImg = pygame.image.load('ex05/player.png')
 playerX, playerY = 370, 480
 playerX_change = 0
 
 # Enemy
-enemyImg = pygame.image.load('enemy.png')
+enemyImg = pygame.image.load('ex05/enemy.png')
 enemyX = random.randint(0, 736)
 enemyY = random.randint(50, 150)
 enemyX_change, enemyY_change = 4, 40
 
 # Bullet
-bulletImg = pygame.image.load('bullet.png')
+bulletImg = pygame.image.load('ex05/bullet.png')
 bulletX, bulletY = 0, 480
 bulletX_change, bulletY_change = 0, 3
 bullet_state = 'ready'
+
+#ここ
+# Shield
+shield_radius = 80
+shield_color = (218, 12, 95)  # Blue color for the shield
+shield_state = 'ready'
 
 # Score
 score_value = 0
@@ -39,10 +45,14 @@ def fire_bullet(x, y):
     global bullet_state
     bullet_state = 'fire'
     screen.blit(bulletImg, (x + 16, y + 10))
+    
+#ここ
+def draw_shield(x, y):
+    pygame.draw.circle(screen, shield_color, (x + 33, y), shield_radius)
 
-def isCollision(enemyX, enemyY, bulletX, bulletY):
-    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + math.pow(enemyY - bulletY, 2))
-    if distance < 27:
+def isCollision(obj1X, obj1Y, obj2X, obj2Y, obj1_radius, obj2_radius):
+    distance = math.sqrt(math.pow(obj1X - obj2X, 2) + math.pow(obj1Y - obj2Y, 2))
+    if distance < (obj1_radius + obj2_radius):
         return True
     else:
         return False
@@ -64,7 +74,12 @@ while running:
                 if bullet_state is 'ready':
                     bulletX = playerX
                     fire_bullet(bulletX, bulletY)
-
+            #ここ        
+            if event.key == pygame.K_s:  # Press 'S' to activate shield
+                if shield_state == 'ready':
+                    shieldX, shieldY = playerX, playerY
+                    shield_state = 'active'
+                    
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -75,22 +90,26 @@ while running:
     elif playerX >= 736:
         playerX = 736
 
+    #ここ
+    # Shield
+    if shield_state == 'active':
+        draw_shield(playerX, playerY)
+
+
     # Enemy
     if enemyY > 440:
         break
     enemyX += enemyX_change
     if enemyX <= 0: #左端に来たら
-        enemyX_change = 4
+        enemyX_change = 4 
         enemyY += enemyY_change
     elif enemyX >=736: #右端に来たら
         enemyX_change = -4
         enemyY += enemyY_change
 
-    collision = isCollision(enemyX, enemyY, bulletX, bulletY)
-    if collision:
-        bulletY = 480
-        bullet_state = 'ready'
-        score_value += 1
+    #ここ
+    if shield_state == 'active' and isCollision(enemyX, enemyY, playerX, playerY, shield_radius, 32):
+        shield_state = 'ready'
         enemyX = random.randint(0, 736)
         enemyY = random.randint(50, 150)
 
